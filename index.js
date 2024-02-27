5300cfc44483d36d9ce40703a91b7b15125b46cc
Joao Vitor de Paiva Moreira <14224136678@MUZAMBINHO.EDU.BR>
2024-02-27 13:57:29 -0300

    mais macumba

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ var globalBest
 var w = 0.8
 var c1 = 0.1
 var c2 = 0.1
+var wText
+var c1Text
+var c2Text
+var globalBestText
+var stepsSinceBestChange = 0
 function macumba(){
     let canvas = document.getElementById("my-canvas")
     ctx = canvas.getContext('2d')
+    globalBestText = document.getElementById("globalBest")
+    let globalBestManualText = document.getElementById("globalBestManual")
+    wText = document.getElementById("w")
+    c1Text = document.getElementById("c1")
+    c2Text = document.getElementById("c2")
     let numberOfFlies = 30
+    let globaBestManualPosition = [0,0]
     values = []
     minValue = 1000000
     maxValue = 0
@@ -21,14 +32,31 @@ function macumba(){
     for(let i=0;i<numberOfFlies;i++){
         let randomX = Math.random()*500
         let randomY = Math.random()*500
+        let randomVX = Math.random()*10
+        let randomVY = Math.random()*10
+
+        if(Math.random()>0.5){
+            randomVX = randomVX*-1
+        }
+
+        if(Math.random()>0.5){
+            randomVY = randomVY*-1
+        }
+
         flies.push({
             position: [randomX,randomY],
-            velocity: [0,0],
+            velocity: [randomVX,randomVY],
             personalBest: {
                 position: [randomX,randomY],
                 value:getValue(randomX,randomY)
             }
         })
+        if(flies[i].personalBest.value < globalBest.value){
+            globalBest = {
+                position: [flies[i].personalBest.position[0],flies[i].personalBest.position[1]],
+                value: flies[i].personalBest.value
+            }
+        }
     }
 
     for(let i=0;i<=500;i++){
@@ -37,6 +65,8 @@ function macumba(){
             let value = getValue(i,j)
             values[i][j] = value
             if(value<minValue){
+                globaBestManualPosition[0]=i
+                globaBestManualPosition[1]=j
                 minValue = value
             }
             if(value>maxValue){
@@ -45,9 +75,13 @@ function macumba(){
             
         }
     }
-
-    console.log('macumba')
-    console.log(flies)
+    globalBestManualText.innerText = "Melhor ponto encontrado por busca exaustiva: "+globaBestManualPosition[0]+","+globaBestManualPosition[1]+" "+minValue
+    wText.value = w
+    c1Text.value = c1
+    c2Text.value = c2
+    //console.log(minValue)
+    // console.log('macumba')
+    // console.log(flies)
     
     drawAll(ctx,values,flies,minValue,maxValue)
     //updateFlies();
@@ -56,9 +90,9 @@ function macumba(){
 function updateFlies(step = false){
     while(run){
         for(let i of flies.keys()){
-            console.log('start update')
-            console.log(i)
-            console.log(flies[i])
+            // console.log('start update')
+            // console.log(i)
+            // console.log(flies[i])
             let current = getValue(flies[i].position[0],flies[i].position[1])
             if(current < flies[i].personalBest.value){
                 flies[i].personalBest = {
@@ -67,23 +101,33 @@ function updateFlies(step = false){
                 }
             }
             if(flies[i].personalBest.value < globalBest.value){
-                console.log("mudou melhor")
+                // console.log("mudou melhor")
+                stepsSinceBestChange=0
                 globalBest = {
                     position: [flies[i].personalBest.position[0],flies[i].personalBest.position[1]],
                     value: flies[i].personalBest.value
                 }
             }
             flies[i].velocity = velocidadeDoDemonio(flies[i].velocity,flies[i].position,flies[i].personalBest.position,globalBest)
-            console.log('end update')
-            console.log(flies[i])
+            // console.log('end update')
+            // console.log(flies[i])
             flies[i].position[0] = flies[i].position[0] + flies[i].velocity[0]
             flies[i].position[1] = flies[i].position[1] + flies[i].velocity[1]
             
         }
+        wText.value = w
+        c1Text.value = c1
+        c2Text.value = c2
+        stepsSinceBestChange++;
         //console.log(flies)
         if(step){
             run = false
         }
+        console.log(stepsSinceBestChange)
+        if(stepsSinceBestChange>20){
+            run = false
+        }
+        globalBestText.innerText = "Melhor ponto encontrado por enxame de particulas: "+globalBest.position[0]+","+globalBest.position[1]+" "+globalBest.value
         drawAll(ctx,values,flies,minValue,maxValue)
     }
 }
@@ -97,10 +141,16 @@ function continueFlies(){
 }
 
 function step(){
-    console.log('step')
-    console.log(flies)
+    // console.log('step')
+    // console.log(flies)
     run = true
     updateFlies(true);
+}
+
+function updateWeight(){
+    w = wText.value
+    c1 = c1Text.value
+    c2 = c2Text.value
 }
 
 function drawAll(ctx,values,flies,minValue,maxValue){
@@ -131,8 +181,8 @@ function drawAll(ctx,values,flies,minValue,maxValue){
     ctx.fillStyle = "red";
     ctx.fill();
 
-    console.log('endDraw')
-    console.log(flies)
+    // console.log('endDraw')
+    // console.log(flies)
 }
 
 function drawFly(ctx,fly){
@@ -147,6 +197,14 @@ function drawFly(ctx,fly){
     ctx.arc(fly.position[0],fly.position[1], 3, 0, 2 * Math.PI);
     ctx.fillStyle = "blue";
     ctx.fill();
+
+    //draw velocity
+    ctx.beginPath();
+    ctx.moveTo(fly.position[0],fly.position[1]);
+    ctx.lineTo(fly.position[0] + fly.velocity[0],fly.position[1] + fly.velocity[1]);
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 1;
+    ctx.stroke();
 }
 
 function clearCanvas(ctx){
@@ -188,12 +246,12 @@ function velocidadeDoDemonio(velocity,currentPos,pbest,gbest){
     let newXVelocity = w*velocity[0]+c1*r1*(pbest[0]-currentPos[0])+c2*r2*(gbest.position[0]-currentPos[0])
     let newYVelocity = w*velocity[1]+c1*r1*(pbest[1]-currentPos[1])+c2*r2*(gbest.position[1]-currentPos[1])
     let newVelocity = [newXVelocity,newYVelocity]
-    console.log("newVelocity")
-    console.log(velocity)
-    console.log(currentPos)
-    console.log(pbest)
-    console.log(gbest)
-    console.log(newVelocity)
+    // console.log("newVelocity")
+    // console.log(velocity)
+    // console.log(currentPos)
+    // console.log(pbest)
+    // console.log(gbest)
+    // console.log(newVelocity)
     return newVelocity
 }
 
@@ -201,5 +259,5 @@ function test(){
     let a = [1,1]
     let b = [3,5]
     let c = [a[0]+b[0],a[1]+b[1]]
-    console.log(c)
+    // console.log(c)
 }
